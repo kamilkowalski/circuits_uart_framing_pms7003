9ad774ae68cc8ddb615a4c2ff300d7e54b18d617
Kamil Kowalski <kamil@kamilkowalski.pl>
2020-03-23 21:42:30 +0100

    Initialized Elixir project

---
+++ lib/circuits_uart_framing_pms7003.ex
@@ -0,0 +1,18 @@
+defmodule CircuitsUartFramingPms7003 do
+  @moduledoc """
+  Documentation for `CircuitsUartFramingPms7003`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CircuitsUartFramingPms7003.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/circuits_uart_framing_pms7003_test.exs
@@ -0,0 +1,8 @@
+defmodule CircuitsUartFramingPms7003Test do
+  use ExUnit.Case
+  doctest CircuitsUartFramingPms7003
+
+  test "greets the world" do
+    assert CircuitsUartFramingPms7003.hello() == :world
+  end
+end
