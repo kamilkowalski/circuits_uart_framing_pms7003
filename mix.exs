5d8b61afe14b150abfb2a94153450d9c964b40fd
Kamil Kowalski <kamil@kamilkowalski.pl>
2020-03-28 13:09:26 +0100

    Prepare package for release

--- mix.exs
+++ mix.exs
@@ -6,14 +6,10 @@ defmodule CircuitsUARTFramingPMS7003.MixProject do
       app: :circuits_uart_framing_pms7003,
       version: "0.1.0",
       elixir: "~> 1.9",
-      start_permanent: Mix.env() == :prod,
-      deps: deps()
-    ]
-  end
-
-  def application do
-    [
-      extra_applications: [:logger]
+      source_url: "https://github.com/kamilkowalski/circuits_uart_framing_pms7003",
+      deps: deps(),
+      description: description(),
+      package: package()
     ]
   end
 
@@ -21,6 +17,19 @@ defmodule CircuitsUARTFramingPMS7003.MixProject do
     [
       {:circuits_uart, "~> 1.4"},
       {:ex_doc, "~> 0.21", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description do
+    """
+    Framing module for PMS7003 implementing Circuits.UART.Framing behaviour.
+    """
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/kamilkowalski/circuits_uart_framing_pms7003"}
     ]
   end
 end
